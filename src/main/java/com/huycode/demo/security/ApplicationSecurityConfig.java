0ab3cf9c22c54de8ef3111f41276a5d6c55d9140
huy190302 <huykst137@gmail.com>
2021-09-25 16:15:11 +0700

    closes #9. completed JWT Token verifier filter

--- src/main/java/com/huycode/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/huycode/demo/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.huycode.demo.security;
 
 import com.huycode.demo.auth.ApplicationUserService;
+import com.huycode.demo.jwt.JwtTokenVerifier;
 import com.huycode.demo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -42,6 +43,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
